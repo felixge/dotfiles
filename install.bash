170851dd9717a89a8c1d4af7fd2b700c72d0ba4b
Felix Geisendörfer <felix@debuggable.com>
2016-09-06 15:25:31 +0200

    Improve install.bash

--- install.bash
+++ install.bash
@@ -5,21 +5,14 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files=$(git ls-tree --name-only HEAD | grep -v 'install.bash')
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in ${files}; do
-    echo "file: $file"
-    #echo "Moving any existing dotfiles from ~ to $olddir"
-    #mv ~/.$file ~/dotfiles_old/
-    #echo "Creating symlink to $file in home directory."
-    #ln -s $dir/$file ~/.$file
+  if [[ -f ~/"${file}" && ( ! -L ~/"${file}" ) ]]; then
+    echo "Creating backup in $olddir/$file"
+    mv ~/"$file" "$olddir/$file"
+  fi
+
+  echo "Symlinking ~/$file"
+  ln -fs $dir/$file ~/$file
 done
