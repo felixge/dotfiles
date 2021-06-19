73a8e789ab85d34eb8ca84e3cde6400887bca5cc
Felix Geisendörfer <felix@felixge.de>
2021-06-19 13:15:33 +0200

    simplify telescope config

--- vim/.config/nvim/telescope.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-M.search_dotfiles = function()
-    require("telescope.builtin").find_files({
-        prompt_title = "< VimRC >",
-        cwd = "$HOME/go/src/github.com/felixge/dotfiles/vim",
-    })
-end
