c8bd4eb59af2eb6251074269c0f5f3bd245d4e4e
Felix Geisendörfer <felix@felixge.de>
2025-12-13 13:04:11 +0100

    assume light mode by default

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.background = "light"
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
