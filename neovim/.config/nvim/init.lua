7dfdcd569213417b4e53f786f774a1fc324ff195
Felix Geisendörfer <felix@felixge.de>
2026-03-05 15:31:48 +0100

    fix(vim): add go treesitter parser for syntax highlighting
    Prompts:
    
    > I'm not seeing any syntax highlighting when I open go files?

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -870,7 +870,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
