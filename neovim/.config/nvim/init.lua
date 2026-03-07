ab9f6bb2723764cd673d049790e5e6abcb184e91
Felix Geisendörfer <felix@felixge.de>
2026-03-07 09:20:44 +0100

    feat(neovim): add LSP and treesitter support for Python, Rust, and TypeScript
    Enable pyright, rust_analyzer, and ts_ls language servers via Mason.
    Add treesitter parsers for python, rust, javascript, typescript, and tsx.
    
    Prompts:
    
    > Does my vim config have support for python and rust enabled? Including language servers? If not, please do it. Also do JS / TypeScript.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -659,14 +659,9 @@ require('lazy').setup({
             cb(vim.fs.root(bufnr, go_root_markers))
           end,
         },
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        pyright = {},
+        rust_analyzer = {},
+        ts_ls = {},
 
         stylua = {}, -- Used to format Lua code
 
@@ -939,7 +934,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'diff', 'go', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'tsx', 'typescript', 'vim', 'vimdoc' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
