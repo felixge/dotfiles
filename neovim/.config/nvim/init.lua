bc5f286d6aeffc743dd73c8b4104945de428f092
Felix Geisendörfer <felix@felixge.de>
2026-03-08 18:08:41 +0000

    feat(vim): enable formatting for python

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -797,7 +797,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         go = { 'goimports' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
