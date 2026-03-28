6b9c0f56944da603709de92813e42c84efdaa373
Felix Geisendörfer <felix@felixge.de>
2026-03-28 15:03:53 +0100

    chore(vim): get rid of link plugin

--- neovim/.config/nvim/lua/custom/plugins/init.lua
+++ neovim/.config/nvim/lua/custom/plugins/init.lua
@@ -15,24 +15,7 @@ return {
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-rhubarb' },
   { 'tpope/vim-surround' },
-  {
-    'qadzek/link.vim',
-    ft = 'markdown',
-    init = function() vim.g.link_heading = '' end,
-    config = function()
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = '*.md',
-        callback = function() vim.cmd 'LinkConvertAll' end,
-      })
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'markdown',
-        callback = function(ev)
-          vim.b.link_skip_line = '!\\['
-          vim.keymap.set('n', 'gx', '<cmd>LinkOpen<cr>', { buffer = ev.buf, desc = 'Open link under cursor' })
-        end,
-      })
-    end,
-  },
+
   -- {
   --   'preservim/vim-markdown',
   --   ft = 'markdown',
