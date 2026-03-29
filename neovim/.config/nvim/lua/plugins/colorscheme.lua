40285a1116b2c2ff801b3eb645486397bbb7896e
Felix Geisendörfer <felix@felixge.de>
2026-03-29 18:50:04 +0200

    chore(vim): better code background column

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
       require("onedark").setup({
         highlights = {
           ["Whitespace"] = { fg = "#eaeaea" },
-          ["ColorColumn"] = { bg = "#ff0000" },
+          ["ColorColumn"] = { bg = "#f0f0f0" },
         },
       })
       require("onedark").load()
