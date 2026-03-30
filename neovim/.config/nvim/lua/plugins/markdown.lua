09e14d6dc0045aa599319d6b84256141275c52f2
Felix Geisendörfer <felix@felixge.de>
2026-03-30 13:09:54 +0200

    feat(vim): turn inline links into footer links automatically

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,23 @@
 return {
   {
+    'qadzek/link.vim',
+    ft = 'markdown',
+    init = function() vim.g.link_heading = '' end,
+    config = function()
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.md',
+        callback = function() vim.cmd 'LinkConvertAll' end,
+      })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function(ev)
+          vim.b.link_skip_line = '!\\['
+          vim.keymap.set('n', 'gx', '<cmd>LinkOpen<cr>', { buffer = ev.buf, desc = 'Open link under cursor' })
+        end,
+      })
+    end,
+  },
+  {
     "HakonHarnes/img-clip.nvim",
     event = "VeryLazy",
     opts = {},
