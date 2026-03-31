ce8f8c8941bef70d59f17e9c00d0c3e40d9ca052
Felix Geisendörfer <felix@felixge.de>
2026-03-31 10:14:51 +0200

    chore(neovim): disable blink.cmp auto-show and ghost text
    Prompts:
    
    > My vim config is automatically suggesting a lot of completion ideas while I'm typing. I find this annoying. Disable it.
    
    > Ok, the menu is now gone, but I'm still seeing inline suggestions for type-ahead.
    
    > activate the menu again for a second
    
    > no disable it again
    
    > commit

---
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        auto_show = false,
+      },
+      ghost_text = {
+        enabled = false,
+      },
+    },
+  },
+}
