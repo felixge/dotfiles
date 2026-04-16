4ee7ed48afd9dbc7850d0d379234fca45113f930
Felix Geisendörfer <felix@felixge.de>
2026-04-16 18:13:11 -0400

    feat(vim): commit message text limits

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Commit message line length: 72 cols for the body (subject line handled visually)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'gitcommit', 'jjdescription' },
+  callback = function()
+    vim.opt_local.textwidth = 72
+    vim.opt_local.colorcolumn = '51,73'
+    vim.opt_local.spell = true
+    -- Let `gq` use Vim's built-in wrapper, not LSP formatexpr
+    vim.opt_local.formatexpr = ''
+  end,
+})
+
